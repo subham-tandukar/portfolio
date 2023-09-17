907eafa49087dfd780ac826585af3a50f21d66d4
Subham <subhamtndkr9@gmail.com>
2023-09-17 22:14:00 +0545

    layout pushed

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,17 @@
-// "use client";
+"use client";
 import Header from "@/component/header";
 import "./globals.css";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import { ThemeContextProvider } from "@/context/theme-context";
 import Theme from "@/component/theme";
-// import { Courgette, Comic_Neue } from "next/font/google";
-// const courgette = Courgette({ weight: "400", subsets: ["latin"] });
-// const comicNeue = Comic_Neue({
-//   subsets: ["latin"],
-//   weight: ["300", "400", "700"],
-// });
+import { Courgette, Comic_Neue } from "next/font/google";
+const courgette = Courgette({ weight: "400", subsets: ["latin"] });
+const comicNeue = Comic_Neue({
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+});
 
-export const metadata = {
+const metadata = {
   title: "Subham | Personal Portfolio",
   description:
     "Subham is a frontend web developer with more than 1 year of experience",
@@ -20,12 +20,12 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      {/* <style jsx global>{`
+      <style jsx global>{`
         :root {
           --comicNeue-font: ${comicNeue.style.fontFamily};
           --courgette-font: ${courgette.style.fontFamily};
         }
-      `}</style> */}
+      `}</style>
       <body>
         <div className="bg-1"></div>
         <div className="bg-2"></div>
