f8aa9e03bde96ba1e6837c16a55c6ed82cc001a8
Subham <subhamtndkr9@gmail.com>
2023-09-17 22:04:14 +0545

    header pushed

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,15 @@
-"use client";
+// "use client";
 import Header from "@/component/header";
 import "./globals.css";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import { ThemeContextProvider } from "@/context/theme-context";
 import Theme from "@/component/theme";
-import { Courgette, Comic_Neue } from "next/font/google";
-const courgette = Courgette({ weight: "400", subsets: ["latin"] });
-const comicNeue = Comic_Neue({
-  subsets: ["latin"],
-  weight: ["300", "400", "700"],
-});
+// import { Courgette, Comic_Neue } from "next/font/google";
+// const courgette = Courgette({ weight: "400", subsets: ["latin"] });
+// const comicNeue = Comic_Neue({
+//   subsets: ["latin"],
+//   weight: ["300", "400", "700"],
+// });
 
 export const metadata = {
   title: "Subham | Personal Portfolio",
@@ -20,12 +20,12 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <style jsx global>{`
+      {/* <style jsx global>{`
         :root {
           --comicNeue-font: ${comicNeue.style.fontFamily};
           --courgette-font: ${courgette.style.fontFamily};
         }
-      `}</style>
+      `}</style> */}
       <body>
         <div className="bg-1"></div>
         <div className="bg-2"></div>
